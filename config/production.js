b3b034ae9200b9487ab7ff0939350803e7d985ba
Sam Breed <sam@quickleft.com>
2015-02-03 15:49:43 -0800

    adding production config

---
+++ config/production.js
@@ -0,0 +1,4 @@
+exports.sprintly_api_root = 'https://local.sprint.ly:9000';
+exports.client_id = '71b04d57289d7351ec1a';
+exports.client_secret = 'f40c866ae0b38a5d2907acc278f3d6c08c505b8a';
+exports.cookie_password = 'very,very,very,very,very,secret!';
