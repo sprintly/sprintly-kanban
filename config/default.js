9e70345dd8b0ab9e7c594d9523cd65049fc643cf
Sam Breed <sam@quickleft.com>
2015-02-04 11:07:21 -0800

    updates for dynamic config

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 exports.sprintly_api_root = 'https://local.sprint.ly:9000';
-exports.client_id = 'c852f0fbfa224a66c5d7';
-exports.client_secret = '18698799e3ca8de9c5c331364a43f22dcec880b2';
-exports.cookie_password = 'shhh';
+exports.client_id = process.env.SPRINTLY_CLIENT_ID || 'c852f0fbfa224a66c5d7';
+exports.client_secret = process.env.SPRINTLY_SECRET || '18698799e3ca8de9c5c331364a43f22dcec880b2';
+exports.cookie_password = process.env.COOKIE_PASSWORD || 'shhh';
+exports.forceHttps = process.env.FORCE_HTTPS || true;
--- config/production.js
+++ config/production.js
@@ -1,4 +1,1 @@
-exports.sprintly_api_root = 'https://local.sprint.ly:9000';
-exports.client_id = '***REMOVED***';
-exports.client_secret = '***REMOVED***';
-exports.cookie_password = 'very,very,very,very,very,secret!';
+exports.sprintly_api_root = 'https://sprint.ly/';
