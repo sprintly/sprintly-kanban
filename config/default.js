461be1d66d1e246176dbe25ac3ccb3519f8b02ec
Sam Breed <sam@quickleft.com>
2015-02-05 11:27:27 -0800

    try setting redirect_uri manually

--- config/default.js
+++ config/default.js
@@ -1,5 +1,6 @@
 exports.sprintly_api_root = 'https://local.sprint.ly:9000';
-exports.client_id = process.env.SPRINTLY_CLIENT_ID || 'c852f0fbfa224a66c5d7';
-exports.client_secret = process.env.SPRINTLY_SECRET || '18698799e3ca8de9c5c331364a43f22dcec880b2';
+exports.client_id = process.env.SPRINTLY_CLIENT_ID || '***REMOVED***';
+exports.client_secret = process.env.SPRINTLY_SECRET || '***REMOVED***';
 exports.cookie_password = process.env.COOKIE_PASSWORD || 'shhh';
 exports.forceHttps = process.env.FORCE_HTTPS || true;
+exports.redirect_uri = 'http://local.sprint.ly:3600/login'
