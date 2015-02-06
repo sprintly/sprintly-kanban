34586607b56bb2e13fee5d167040ebe591051379
Sam Breed <sam@quickleft.com>
2015-02-05 23:43:16 -0800

    fixing a typo in config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 exports.sprintly_api_root = 'https://local.sprint.ly:9000';
 exports.client_id = process.env.SPRINTLY_CLIENT_ID || '71b04d57289d7351ec1a';
-exports.client_secret = process.env.SPRINTLY_SECRET || 'f40c866ae0b38a5d2907acc278f3d6c08c505b8a';
+exports.client_secret = process.env.SPRINTLY_CLIENT_SECRET || 'f40c866ae0b38a5d2907acc278f3d6c08c505b8a';
 exports.cookie_password = process.env.COOKIE_PASSWORD || 'shhh';
 exports.forceHttps = process.env.FORCE_HTTPS || true;
 exports.redirect_uri = 'http://local.sprint.ly:3600/login'
