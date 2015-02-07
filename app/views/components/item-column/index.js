0b9397098a3b224970146946bf2dce1b48765107
Sam Breed <sam@quickleft.com>
2015-02-06 23:00:21 -0800

    adding column headers and show / hide outer columns

--- app/views/components/item-column/index.js
+++ app/views/components/item-column/index.js
@@ -3,7 +3,7 @@
  */
 
 var _ = require('lodash')
-var React = require('react')
+var React = require('react/addons')
 var ItemCard = require('../item-card')
 var Backbone = require('backdash');
 var Header = require('./header')
@@ -20,7 +20,9 @@ var ItemColumn = React.createClass({
 
   getInitialState: function() {
     return {
-      isLoading: true
+      isLoading: true,
+      headerOffset: 0,
+      scrolling: false
     }
   },
 
@@ -35,15 +37,12 @@ var ItemColumn = React.createClass({
     });
 
     switch(this.props.status) {
-      case 'someday':
-        this.items.config.set({ limit: 50 });
-        break;
-
       case 'accepted':
         this.items.config.set({ limit: 5 });
         break;
 
       default:
+        this.items.config.set({ limit: 50 });
         break;
     }
 
@@ -61,18 +60,47 @@ var ItemColumn = React.createClass({
     }
   },
 
+  setHeaderOffset: _.debounce(function() {
+    this.setState({
+      headerOffset: this.getDOMNode().scrollTop,
+      scrolling: false
+    })
+  }, 300),
+
+  hideHeader: _.throttle(function() {
+    this.setState({
+      scrolling: true
+    })
+  }, 20),
+
+  onScroll: function() {
+    this.setState({
+      scrolling: true
+    })
+    this.setHeaderOffset();
+  },
+
   render: function() {
     var limit = this.items.length;
-    var classes = "column " + this.props.status;
+    var classes = {
+      column: true,
+      [this.props.status]: true,
+      scrolling: this.state.scrolling
+    };
 
     return (
-      <div className={classes}>
+      <div className={React.addons.classSet(classes)} {...this.props} onScroll={_.throttle(this.onScroll, 30)}>
+        <header style={{ top: this.state.headerOffset }}>
+          {this.props.status}
+        </header>
+        <div className="column__inner">
         {this.state.isLoading ?
           <div className="loading"><Loading type="bubbles" color="#ccc"/></div> :
           this.items.map(function(model, index) {
             return <ItemCard item={model} key={model.id} />
           })
         }
+        </div>
       </div>
     )
   }
