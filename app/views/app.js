7257259092400115db07f5c6f518940c8d8f8a28
Sam Breed <sam@quickleft.com>
2015-02-11 16:40:45 -0800

    removing non-flux data fetches from app view

--- app/views/app.js
+++ app/views/app.js
@@ -3,27 +3,9 @@ import { RouteHandler } from "react-router";
 import Promise from "bluebird";
 
 export default React.createClass({
-
-  propTypes: {
-    user: React.PropTypes.object.isRequired,
-    products: React.PropTypes.object.isRequired
-  },
-
-  componentDidMount: function() {
-    Promise.all([
-      this.props.user.fetch(),
-      this.props.products.fetch()
-    ]).then(() => {
-      this.forceUpdate();
-    });
-  },
-
   render: function() {
     return (
-      <div>
-        <RouteHandler {...this.props} />
-      </div>
+      <RouteHandler {...this.props} />
     )
   }
-
 });
