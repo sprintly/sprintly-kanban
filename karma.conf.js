d88c5c1d599f5226e1cefc779a3bef57344fa6ff
Sam Breed <sam@quickleft.com>
2015-03-17 11:12:56 -0700

    adding Firefox test runner for CI

--- karma.conf.js
+++ karma.conf.js
@@ -74,6 +74,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    browserNoActivityTimeout: 30e3,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
