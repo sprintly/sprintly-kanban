54b71a846a8417740fa7bb5f512fd04bde140fba
Sam Breed <sam@quickleft.com>
2015-03-23 00:02:13 -0700

    use bootstrap button dropdown for column sorting

--- app/views/components/item-column/header.js
+++ app/views/components/item-column/header.js
@@ -1,54 +1,52 @@
 var _ = require('lodash');
 var React = require('react/addons');
+var Bootstrap = require('react-bootstrap');
 
-var defaultSortOptions = {
-  'Created': 'created_at',
-  'Recent': 'last_modified',
-};
 
 const SORT_OPTIONS = {
-  someday: _.extend({
-    'Priority': 'priority',
-  }, defaultSortOptions),
-  backlog: _.extend({
-    'Priority': 'priority',
-  }, defaultSortOptions),
-  'in-progress': _.extend({
-    'Priority': 'priority',
-  }, defaultSortOptions),
-  completed: _.extend({
-    // 'Complete': 'progress.closed_at'
-  }, defaultSortOptions),
-  accepted: _.extend({
-    // 'Accepted': 'progress.accepted_at'
-  }, defaultSortOptions)
+  created_at: 'Created At',
+  priority: 'Priority',
+  last_modified: 'Recent'
 };
 
-
 var Header = React.createClass({
-  render: function() {
 
-    var directionClasses = {
+  getDefaultProps: function() {
+    return {
+      sortField: 'last_modified'
+    }
+  },
+
+  onSelect: function(field) {
+    this.props.setSortCriteria(field);
+  },
+
+  onReverseClick: function() {
+    if (this.props.sortField !== 'priority') {
+      this.props.reverse();
+    }
+  },
+
+  render: function() {
+    let directionClasses = {
       'glyphicon': true,
       'glyphicon-sort-by-attributes': this.props.sortDirection === 'desc',
       'glyphicon-sort-by-attributes-alt': this.props.sortDirection === 'asc',
-      'hidden': this.props.sortField === 'priority'
     };
 
     return (
       <header>
         <div className="column__sort-options">
-          {_.map(SORT_OPTIONS[this.props.status], function(field, label) {
-            let handler = (ev) => this.props.setSortCriteria(field);
-            let active = field === this.props.sortField ? 'js-active' : '';
-            var classes = `btn btn-default ${active}`;
-            return (
-              <button className={classes} onClick={handler} key={`sort-options-${this.props.status}-${field}`}>
-                {label}
-              </button>
-            );
-          }, this)}
-          <span onClick={this.props.reverse} className={React.addons.classSet(directionClasses)}/>
+          <Bootstrap.SplitButton onSelect={this.onSelect} activeKey={this.props.sortField} bsSize="small" title={`Sort by: ${SORT_OPTIONS[this.props.sortField]}`} pullRight>
+            {_.map(_.omit(SORT_OPTIONS, this.props.sortField), function(label, field) {
+              return (
+                <Bootstrap.MenuItem eventKey={field} key={field}>{label}</Bootstrap.MenuItem>
+              );
+            }, this)}
+          </Bootstrap.SplitButton>
+          <button className="reverse-sort" disabled={this.props.sortField === 'priority'} type="button" onClick={this.onReverseClick} aria-label="Change sort direction">
+            <span aria-hidden="true" className={React.addons.classSet(directionClasses)}/>
+          </button>
         </div>
       </header>
     )
