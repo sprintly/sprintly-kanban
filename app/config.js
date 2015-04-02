6b0deaf2ae7be11288763e525164ee8406f593f6
Sam Breed <sam@quickleft.com>
2015-04-02 14:42:54 -0700

    move pusher keys to config

---
+++ app/config.js
@@ -0,0 +1,4 @@
+exports.CHANNEL_PREFIX = process.NODE_ENV === 'production' ?
+  'api-product_sprintly-aws' : 'api-product_sprintly-development-justinlilly';
+
+exports.PUSHER_KEY = '97b75cf8a05a1faa7ee7';
