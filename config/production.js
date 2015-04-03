5f49e8235df5f6796bb39f41c8cd29e7954eb39c
Sam Breed <sam@quickleft.com>
2015-04-02 17:07:29 -0700

    allow for configurable redirect_uri in production

--- config/production.js
+++ config/production.js
@@ -1,2 +1,2 @@
 exports.sprintly_api_root = 'https://sprint.ly';
-exports.redirect_uri = 'https://sprintly-manifold.herokuapp.com/login'
+exports.redirect_uri = process.env.REDIRECT_URI || 'https://sprintly-manifold.herokuapp.com/login'
