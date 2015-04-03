781322a2e800c41d515be4821e30837657a9c615
Sam Breed <sam@quickleft.com>
2015-04-02 21:30:12 -0700

    factoring out Gravatar component

---
+++ app/views/components/gravatar.js
@@ -0,0 +1,32 @@
+var React = require('react/addons');
+var crypto = require('crypto');
+
+var Gravatar = React.createClass({
+
+  propTypes: {
+    email: React.PropTypes.string,
+    size: React.PropTypes.number
+  },
+
+  getDefaultProps: function() {
+    return {
+      size: 36
+    };
+  },
+
+  getAvatar: function(email) {
+    var hash = crypto.createHash('md5')
+    hash.update(this.props.email.toLowerCase().trim())
+    var url = `//www.gravatar.com/avatar/${hash.digest('hex')}.jpg\
+      ?d=identicon&s=${this.props.size}`;
+    return <img src={url} {...this.props} />;
+  },
+
+  render: function() {
+    return this.props.email != null ?
+      this.getAvatar(this.props.email) : '';
+  }
+});
+
+module.exports = Gravatar;
+
