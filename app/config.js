e8b32d8709599fd01756f1d60cc8bb00cd91c614
Sam Breed <sam@quickleft.com>
2015-04-05 19:34:27 -0700

    fix pusher config for production

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
-exports.CHANNEL_PREFIX = process.NODE_ENV === 'production' ?
+exports.CHANNEL_PREFIX = process.env.NODE_ENV === 'production' ?
   'api-product_sprintly-aws' : 'api-product_sprintly-development-justinlilly';
 
 exports.PUSHER_KEY = '***REMOVED***';
