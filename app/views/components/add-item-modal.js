6d2dcf16466b9b9d9e49a34463f5416f5a74e65a
Sam Breed <sam@quickleft.com>
2015-04-09 23:44:13 -0700

    adding mention support and more styling for modal

--- app/views/components/add-item-modal.js
+++ app/views/components/add-item-modal.js
@@ -1,12 +1,74 @@
 import _ from 'lodash';
 import React from 'react/addons';
 import {Modal,Nav,NavItem} from 'react-bootstrap';
+import {MentionsInput, Mention} from 'react-mentions';
 
 var AddItemModal = React.createClass({
   getInitialState() {
     return {
-      activeTab: 'story'
+      type: 'story',
+      description: ""
     }
+  },
+
+  getDefaultProps() {
+    return {
+      members: [
+        {
+          id: 'John Doe',
+          display: 'John Doe'
+        }
+      ]
+    }
+  },
+
+  onChange(ev, value) {
+    this.setState({ description: value });
+  },
+
+  onSelect(type) {
+    this.setState({ type: type });
+  },
+
+  renderStoryTitle() {
+
+    let who = (
+      <div className="add-item__field who">
+        <span>As an</span>
+        <div className="input-group">
+          <label>Who</label>
+          <input className="form-control" type="text" name="who" placeholder="e.g. accountant"/>
+        </div>
+      </div>
+    );
+
+    let what = (
+      <div className="add-item__field what">
+        <span>I want</span>
+        <div className="input-group">
+          <label>What</label>
+          <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration"/>
+        </div>
+      </div>
+    );
+
+    let why = (
+      <div className="add-item__field why">
+        <span>so that</span>
+        <div className="input-group">
+          <label>Why</label>
+          <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration"/>
+        </div>
+      </div>
+    );
+
+    return [who, what, why];
+  },
+
+  renderTitle() {
+    return (
+      <input className="form-control" placeholder="What is it?" name="title"/>
+    );
   },
 
   render() {
@@ -18,45 +80,44 @@ var AddItemModal = React.createClass({
     ];
 
     return (
-      <Modal {...this.props} backdrop={false} animation={false} className="add-item">
-        <Nav className="add-item__tabs" bsStyle='tabs' activeKey={this.state.activeTab}>
+      <Modal {...this.props} className="add-item">
+        <Nav className="add-item__tabs" bsStyle='tabs' activeKey={this.state.type} onSelect={this.onSelect}>
           {_.map(NAV_ITEMS, function(item) {
-            return <NavItem eventKey={item.type}>{item.label}</NavItem>
+            return (
+              <NavItem eventKey={item.type} className={`add-item__nav-${item.type}`}>
+                {item.label}
+              </NavItem>
+            )
           })}
         </Nav>
         <div className="modal-body">
           <form>
             <div className="form-group">
-              <div className="add-item__field who">
-                <span>As an</span>
-                <div className="input-group">
-                  <label>Who</label>
-                  <input className="form-control" type="text" name="who" placeholder="e.g. accountant"/>
-                </div>
-              </div>
-              <div className="add-item__field what">
-                <span>I want</span>
-                <div className="input-group">
-                  <label>What</label>
-                  <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration"/>
-                </div>
-              </div>
-              <div className="add-item__field why">
-                <span>so that</span>
-                <div className="input-group">
-                  <label>Why</label>
-                  <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration"/>
-                </div>
-              </div>
+            {this.state.type === 'story' ? this.renderStoryTitle() : this.renderTitle()}
             </div>
             <div className="form-group">
-              <textarea className="form-control" placeholder="Add a description and drag files here to attach..."></textarea>
+              <MentionsInput value={this.state.description} onChange={this.onChange} placeholder="Add a description and drag files here to attach...">
+                <Mention data={this.props.members} />
+              </MentionsInput>
             </div>
             <div className="form-group">
               <input className="form-control" type="text" name="tags" placeholder="Tags"/>
             </div>
-            <button className="btn btn-primary">Create Item</button>
-            <button className="btn btn-default">Cancel</button>
+
+            <div className="row">
+              <div className="col-sm-7">
+              </div>
+              <div className="col-sm-5 add-item__actions">
+                <button className="btn btn-primary btn-lg">Create Item</button>
+                <button className="btn btn-default btn-lg">Cancel</button>
+                <div className="checkbox">
+                  <label>
+                    <input type="checkbox" name="backlog" checked/>
+                    Automatically send to backlog.
+                  </label>
+                </div>
+              </div>
+            </div>
           </form>
         </div>
       </Modal>
