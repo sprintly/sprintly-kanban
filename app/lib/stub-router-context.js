1e7d7b30192251d0738173993822942ff637b932
Sam Breed <sam@quickleft.com>
2015-04-13 22:43:30 -0700

    factor router context stub into a module

---
+++ app/lib/stub-router-context.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import React from 'react/addons';
+
+var stubRouterContext = (Component, props, stubs) => {
+  return React.createClass({
+    childContextTypes: {
+      getCurrentPath: React.PropTypes.func,
+      getCurrentRoutes: React.PropTypes.func,
+      getCurrentPathname: React.PropTypes.func,
+      getCurrentParams: React.PropTypes.func,
+      getCurrentQuery: React.PropTypes.func
+    },
+
+    getChildContext: function() {
+      return _.extend({
+        getCurrentPath () {},
+        getCurrentRoutes () {},
+        getCurrentPathname () {},
+        getCurrentParams () {},
+        getCurrentQuery () {}
+      }, stubs);
+    },
+
+    render: function() {
+      return <Component {...props} />;
+    }
+  });
+};
+
+export default stubRouterContext;
