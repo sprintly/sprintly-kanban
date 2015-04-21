2adcfb261234bdd2a160e4373a2c84d8c4e9cf9e
Sam Breed <sam@quickleft.com>
2015-04-20 17:54:43 -0700

    responsive styles for search

--- app/views/components/search-results.js
+++ app/views/components/search-results.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from "react";
-import {Badge, ListGroup, ListGroupItem, ButtonGroup} from 'react-bootstrap';
+import {Badge, ListGroup, ListGroupItem, ButtonGroup, DropdownButton, MenuItem} from 'react-bootstrap';
 import ItemCard from '../components/item-card';
 
 var SearchResults = React.createClass({
@@ -29,7 +29,9 @@ var SearchResults = React.createClass({
   },
 
   addItemType(type, ev) {
-    ev.preventDefault();
+    if (ev) {
+      ev.preventDefault();
+    }
     let itemFacet = `type:${type}`;
     let query = this.addFacet(itemFacet);
     this.props.search(query, { progressBar: false });
@@ -50,25 +52,36 @@ var SearchResults = React.createClass({
     return (
       <div className="row">
         <div className="col-sm-2 search__filters">
-          <ButtonGroup vertical>
-            {_.map(this.props.results.products, function(product) {
-              return <button className="btn btn-default" onClick={_.partial(this.addProduct, product.id)}>{product.name}</button>
-            }, this)}
-          </ButtonGroup>
-          <ListGroup>
-            <ListGroupItem href="#" onClick={_.partial(this.addItemType, 'story')} bsStyle="success">
-              Stories <Badge>{this.props.results.stories.length}</Badge>
-            </ListGroupItem>
-            <ListGroupItem href="#" onClick={_.partial(this.addItemType, 'defect')} bsStyle="warning">
-              Defects <Badge>{this.props.results.defects.length}</Badge>
-            </ListGroupItem>
-            <ListGroupItem href="#" onClick={_.partial(this.addItemType, 'task')}>
-              Tasks <Badge>{this.props.results.tasks.length}</Badge>
-            </ListGroupItem>
-            <ListGroupItem href="#" onClick={_.partial(this.addItemType, 'test')} bsStyle="info">
-              Tests <Badge>{this.props.results.tests.length}</Badge>
-            </ListGroupItem>
-          </ListGroup>
+          <div className="search__filter-group">
+            <i>Filter by Type</i>
+            <ButtonGroup vertical className="hidden-xs">
+              <a href="#" onClick={_.partial(this.addItemType, 'story')} className="btn btn-default story">Story</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'defect')} className="btn btn-default defect">Defect</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'task')} className="btn btn-default task">Task</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'test')} className="btn btn-default test">Test</a>
+            </ButtonGroup>
+            <ButtonGroup justified className="visible-xs-block">
+              <a href="#" onClick={_.partial(this.addItemType, 'story')} className="btn btn-default story">Story</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'defect')} className="btn btn-default defect">Defect</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'task')} className="btn btn-default task">Task</a>
+              <a href="#" onClick={_.partial(this.addItemType, 'test')} className="btn btn-default test">Test</a>
+            </ButtonGroup>
+          </div>
+          <hr className="hidden-xs"/>
+          <div className="search__filter-group">
+            <i>Filter by Product</i>
+            <ButtonGroup vertical className="hidden-xs">
+              {_.map(this.props.results.products, function(product) {
+                return <button className="btn btn-default" onClick={_.partial(this.addProduct, product.id)}>{product.name}</button>
+              }, this)}
+            </ButtonGroup>
+            <DropdownButton block title="Products" className="visible-xs-inline-block" onSelect={(product) => { this.addProduct(product) }}>
+              {_.map(this.props.results.products, function(product) {
+                  return <MenuItem eventKey={product.id}>{product.name}</MenuItem>
+                }, this)}
+            </DropdownButton>
+          </div>
+          <hr className="visible-xs-block"/>
         </div>
         <div className="col-sm-6 search__results">
           {_.map(page, function(item, index) {
@@ -85,8 +98,29 @@ var SearchResults = React.createClass({
             <button onClick={this.nextPage} className="btn btn-block btn-default">Load More</button>
           }
         </div>
-        <div className="col-sm-2">
-          <small><i>{this.props.results.items.length} matching items.</i></small>
+        <div className="col-sm-2 search__counts">
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td>{this.props.results.items.length}</td>
+                <td>matching items</td>
+              </tr>
+              <tr></tr>
+              {_.compact(['stories', 'defects', 'tasks', 'tests'].map((type) => {
+                if (this.props.results[type].length === 0) {
+                  return ''
+                } else {
+                  return (
+                    <tr>
+                      <td>{this.props.results[type].length}</td>
+                      <td>{type}</td>
+                    </tr>
+                  );
+                }
+              }))}
+            </tbody>
+          </table>
           <hr/>
           <p>Learn about our <a href="https://sprint.ly/blog/search-api/" target="_BLANK">Beta Item Search API with Facets</a>.</p>
         </div>
