a7c61ddb11083242d38c64e6db4f6de2e0dd76d1
Shane Rogers <srogers@quickleft.com>
2015-04-21 17:36:34 -0700

    Add ref to stub router component for accessibility

--- app/lib/stub-router-context.js
+++ app/lib/stub-router-context.js
@@ -22,7 +22,8 @@ var stubRouterContext = (Component, props, stubs) => {
     },
 
     render: function() {
-      return <Component {...props} />;
+
+      return <Component ref='stub' {...props} />;
     }
   });
 };
