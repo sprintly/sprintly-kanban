e219827b2300b6a4bc0b1062bc123e9dcf410ff8
Shane Rogers <srogers@quickleft.com>
2015-04-22 14:15:27 -0700

    Pull search result controls up to parent level

--- app/views/pages/search.js
+++ app/views/pages/search.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React from "react";
 
 import {State, Navigation} from "react-router";
-import {ProgressBar} from 'react-bootstrap';
+import {ProgressBar, ButtonGroup, DropdownButton, MenuItem} from 'react-bootstrap';
 import Header from '../components/header';
 import SearchResults from '../components/search-results/index';
 
@@ -35,34 +35,6 @@ var Search = React.createClass({
 
   _onChange() {
     this.setState(getSearchSelectorState());
-  },
-
-  componentDidMount() {
-    SearchStore.addChangeListener(this._onChange);
-    ProductStore.on('change', this._onChange);
-    ProductActions.init();
-    let query = this.getQuery();
-
-    if (query && query.q) {
-      this.setState({ query: query.q });
-      this.updateProgressBar();
-      SearchActions.search(query.q, query.sort, query.order);
-    }
-  },
-
-  componentWillUnmount() {
-    SearchStore.removeChangeListener(this._onChange);
-    ProductStore.off('change', this._onChange);
-  },
-
-  componentWillReceiveProps(nextProps) {
-    let query = this.getQuery();
-
-    if (this.state.loading === false && query && query.q) {
-      this.setState({ query: query.q });
-      this.updateProgressBar();
-      SearchActions.search(query.q, query.sort, query.order);
-    }
   },
 
   search(query, options={ progressBar: true }) {
@@ -154,6 +126,125 @@ var Search = React.createClass({
     )
   },
 
+  ticketTypeControls() {
+    return ([
+      <i>Filter by Type</i>,
+      <ButtonGroup vertical className="hidden-xs">
+        {this.ticketControlLinks()}
+      </ButtonGroup>,
+      <ButtonGroup justified className="visible-xs-block">
+        {this.ticketControlLinks()}
+      </ButtonGroup>
+    ])
+  },
+
+  ticketControlLinks() {
+    return ([
+      <a href="#" onClick={_.partial(this.addItemType, 'story')} className="btn btn-default story">Story</a>,
+      <a href="#" onClick={_.partial(this.addItemType, 'defect')} className="btn btn-default defect">Defect</a>,
+      <a href="#" onClick={_.partial(this.addItemType, 'task')} className="btn btn-default task">Task</a>,
+      <a href="#" onClick={_.partial(this.addItemType, 'test')} className="btn btn-default test">Test</a>
+    ])
+  },
+
+  resultsSummary() {
+    let issueTypes = ['stories', 'defects', 'tasks', 'tests'];
+    let results = this.state.results;
+
+    return (
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>{results.items.length}</td>
+            <td>matching items</td>
+          </tr>
+          <tr></tr>
+          {_.compact(issueTypes.map((type) => {
+            if (results[type].length === 0) {
+              return ''
+            } else {
+              return (
+                <tr>
+                  <td>{results[type].length}</td>
+                  <td>{type}</td>
+                </tr>
+              );
+            }
+          }))}
+        </tbody>
+      </table>
+    )
+  },
+
+  addProduct(value, ev) {
+    let productFacet = `product:${value}`;
+    let query = this.addFacet(productFacet);
+    this.search(query, { progressBar: false });
+  },
+
+  addItemType(type, ev) {
+    if (ev) {
+      ev.preventDefault();
+    }
+    let itemFacet = `type:${type}`;
+    let query = this.addFacet(itemFacet);
+    this.search(query, { progressBar: false });
+  },
+
+  addFacet(facet) {
+    let query = this.getQuery();
+    query = query.q.indexOf(facet) > -1 ? query.q.replace(facet, ''): `${query.q} ${facet}`;
+
+    return query.trim();
+  },
+
+  productControls() {
+    return ([
+      <i>Filter by Product</i>,
+      <ButtonGroup vertical className="hidden-xs">
+        {_.map(this.state.results.products, (product) => {
+          return <button className="btn btn-default" onClick={_.partial(this.addProduct, product.id)}>{product.name}</button>
+        })}
+      </ButtonGroup>,
+      <DropdownButton block title="Products" className="visible-xs-inline-block" onSelect={(product) => { this.addProduct(product) }}>
+        {_.map(this.state.results.products, (product) => {
+          return <MenuItem eventKey={product.id}>{product.name}</MenuItem>
+        })}
+      </DropdownButton>
+    ])
+  },
+
+  // React Functions
+
+  componentDidMount() {
+    SearchStore.addChangeListener(this._onChange);
+    ProductStore.on('change', this._onChange);
+    ProductActions.init();
+    let query = this.getQuery();
+
+    if (query && query.q) {
+      this.setState({ query: query.q });
+      this.updateProgressBar();
+      SearchActions.search(query.q, query.sort, query.order);
+    }
+  },
+
+  componentWillUnmount() {
+    SearchStore.removeChangeListener(this._onChange);
+    ProductStore.off('change', this._onChange);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    let query = this.getQuery();
+
+    if (this.state.loading === false && query && query.q) {
+      this.setState({ query: query.q });
+      this.updateProgressBar();
+      SearchActions.search(query.q, query.sort, query.order);
+    }
+  },
+
   render() {
     return (
       <div>
@@ -165,7 +256,26 @@ var Search = React.createClass({
         <div className="container search">
           {this.searchBar()}
           <hr/>
-          {this.renderResults()}
+          <div className="row">
+            <div className="col-sm-2 search__filters">
+              <div className="search__filter-group">
+                {this.ticketTypeControls()}
+              </div>
+              <hr className="hidden-xs"/>
+              <div className="search__filter-group">
+                {this.productControls()}
+              </div>
+              <hr className="visible-xs-block"/>
+            </div>
+            <div className="col-sm-8 search__results">
+              {this.renderResults()}
+            </div>
+            <div className="col-sm-2 search__counts">
+              {this.resultsSummary()}
+              <hr/>
+              <p>Learn about our <a href="https://sprint.ly/blog/search-api/" target="_BLANK">Beta Item Search API with Facets</a>.</p>
+            </div>
+          </div>
         </div>
       </div>
     );
