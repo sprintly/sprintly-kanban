d374db9f383c5ee335bff1693118b48f298d730f
Shane Rogers <srogers@quickleft.com>
2015-04-29 15:02:34 -0700

    Add issue-template helper file for defect template

---
+++ app/views/components/add-item/issue-templates.js
@@ -0,0 +1,36 @@
+module.exports = {
+  defect: "#### User Story \
+          \
+          As a user I want to get my filtered report emailed to me with just the filtered results and expected get an email with just my results but experienced a report being sent that contained every item in the database. \
+          \
+          #### Steps to reproduce: \
+          \
+          1. Select a date range to filter items in the Reports View \
+          \
+          2. Click the email reports button \
+          \
+          3. Observe the report has all items in it \
+          \
+          #### Actual Results: \
+          The report came with every item \
+          #### Expected Results: \
+          The report should only contain the items I filtered out. \
+          #### URL where bug was observed: \
+          \
+          \
+          #### Screenshot/Screencast: \
+          \
+          \
+          #### Environment: \
+          \
+          - [ ] Local \
+          - [ ] Staging \
+          - [X] Production \
+          \
+          #### Comments: \
+          \
+          #### Reporting Customer Info: \
+          - Email Address: bsannan@dscxn.com \
+          - UV ticket:https://sprintly.uservoice.com/admin/tickets/12338\
+          "
+}
