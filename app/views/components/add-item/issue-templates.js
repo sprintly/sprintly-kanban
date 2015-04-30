e479229c919971bde7907f06e3161ab97c6661ab
Shane Rogers <srogers@quickleft.com>
2015-04-29 17:33:23 -0700

    Update defect issue template formatting

--- app/views/components/add-item/issue-templates.js
+++ app/views/components/add-item/issue-templates.js
@@ -1,36 +1,25 @@
 module.exports = {
-  defect: "#### User Story \
-          \
-          As a user I want to get my filtered report emailed to me with just the filtered results and expected get an email with just my results but experienced a report being sent that contained every item in the database. \
-          \
-          #### Steps to reproduce: \
-          \
-          1. Select a date range to filter items in the Reports View \
-          \
-          2. Click the email reports button \
-          \
-          3. Observe the report has all items in it \
-          \
-          #### Actual Results: \
-          The report came with every item \
-          #### Expected Results: \
-          The report should only contain the items I filtered out. \
-          #### URL where bug was observed: \
-          \
-          \
-          #### Screenshot/Screencast: \
-          \
-          \
-          #### Environment: \
-          \
-          - [ ] Local \
-          - [ ] Staging \
-          - [X] Production \
-          \
-          #### Comments: \
-          \
-          #### Reporting Customer Info: \
-          - Email Address: bsannan@dscxn.com \
-          - UV ticket:https://sprintly.uservoice.com/admin/tickets/12338\
-          "
+  defect: "#### User Story \n\
+\n\
+#### Steps to reproduce: \n\
+\n\
+#### Actual Results: \n\
+\n\
+#### Expected Results: \n\
+\n\
+#### URL where bug was observed: \n\
+\n\
+#### Screenshot/Screencast: \n\
+\n\
+#### Environment: \n\
+- [ ] Local \n\
+- [ ] Staging \n\
+- [X] Production \n\
+\n\
+#### Comments: \n\
+\n\
+#### Reporting Customer Info: \n\
+- Email Address: customer@email.com \n\
+- Ticket URL: https://mycompany.ticketservice.com/admin/tickets/123\n\
+"
 }
