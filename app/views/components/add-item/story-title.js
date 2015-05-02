0d210e40fdc677c8df9e71e9a6c44e1ed6c5acb1
Shane Rogers <srogers@quickleft.com>
2015-05-01 17:28:34 -0700

    Add validation state to story title component

--- app/views/components/add-item/story-title.js
+++ app/views/components/add-item/story-title.js
@@ -1,37 +1,62 @@
+import _ from 'lodash';
 import React from 'react/addons';
+
+const TITLE_ATTRS = {
+  who: {
+    title: "As an",
+    placeholder: "e.g. an accountant"
+  },
+  what: {
+    title: "I Want",
+    placeholder: "e.g. Quickbooks integration"
+  },
+  why: {
+    title: "so that",
+    placeholder: "e.g. I don't have to import CSV's daily"
+  }
+};
+const STORY_ATTRS = ['who', 'what', 'why'];
+
+function toTitleCase(str) {
+  return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+}
 
 let AddItemStoryTitle = React.createClass({
 
   propTypes: {
     who: React.PropTypes.object.isRequired,
     what: React.PropTypes.object.isRequired,
-    why: React.PropTypes.object.isRequired
+    why: React.PropTypes.object.isRequired,
+    validation: React.PropTypes.object.isRequired
+  },
+
+  titleNodes() {
+    return _.map(STORY_ATTRS, (attr) => {
+      var classes = React.addons.classSet({
+        "form-control": true,
+        'invalid': !this.props.validation.value[attr]
+      });
+
+      return (
+        <div className={`add-item__field ${attr}`}>
+          <span>{TITLE_ATTRS[attr].title}</span>
+          <div className="input-group">
+            <label>{toTitleCase(attr)}</label>
+            <input className={classes}
+                        type="text"
+                        name={attr}
+                 placeholder={attr.placeholder}
+                 valueLink={this.props[attr]} />
+          </div>
+        </div>
+      )
+    })
   },
 
   render() {
     return (
       <div className="form-group story-title">
-        <div className="add-item__field who">
-          <span>As an</span>
-          <div className="input-group">
-            <label>Who</label>
-            <input className="form-control" type="text" name="who" placeholder="e.g. accountant" valueLink={this.props.who}/>
-          </div>
-        </div>
-        <div className="add-item__field what">
-          <span>I want</span>
-          <div className="input-group">
-            <label>What</label>
-            <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration" valueLink={this.props.what}/>
-          </div>
-        </div>
-        <div className="add-item__field why">
-          <span>so that</span>
-          <div className="input-group">
-            <label>Why</label>
-            <input className="form-control" type="text" name="what" placeholder="e.g. Quickbooks integration" valueLink={this.props.why}/>
-          </div>
-        </div>
+        {this.titleNodes()}
       </div>
     )
   }
