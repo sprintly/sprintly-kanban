a09e948ca7abb59bda699926d583849e329f7d26
Shane Rogers <srogers@quickleft.com>
2015-05-01 17:36:48 -0700

    Add validation to issue title

--- app/views/components/add-item/title.js
+++ app/views/components/add-item/title.js
@@ -2,12 +2,18 @@ import React from 'react/addons';
 
 let AddItemTitle = React.createClass({
   propTypes: {
-    title: React.PropTypes.object.isRequired
+    title: React.PropTypes.object.isRequired,
+    validation: React.PropTypes.object.isRequired
   },
 
   render() {
+    var classes = React.addons.classSet({
+      "form-group": true,
+      'has-error': !this.props.validation.value['title']
+    });
+
     return (
-      <div className="form-group">
+      <div className={classes}>
         <input className="form-control" placeholder="What is it?" name="title" valueLink={this.props.title}/>
       </div>
     );
