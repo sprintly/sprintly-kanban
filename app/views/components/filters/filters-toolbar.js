b0a233581cdc290b2f668c388f4aa844e4599b7a
Ben Lewis <bennlewis@gmail.com>
2015-05-22 10:59:22 -0600

    make velocity configurable per page load
    Refs #9845

--- app/views/components/filters/filters-toolbar.js
+++ app/views/components/filters/filters-toolbar.js
@@ -2,13 +2,15 @@ import _ from 'lodash';
 import Filter from './filters-toolbar-filter';
 import FiltersMenu from './filters-menu';
 import FilterAction from '../../../actions/filter-actions';
+import VelocityActions from '../../../actions/velocity-actions';
 import React from 'react/addons';
 
 var FiltersToolbar = React.createClass({
 
   getInitialState: function() {
     return {
-      showFiltersMenu: false
+      showFiltersMenu: false,
+      showVelocityInput: false
     };
   },
 
@@ -27,13 +29,38 @@ var FiltersToolbar = React.createClass({
     this.setState({ showFiltersMenu: !!this.state.showFiltersMenu });
   },
 
+  changeVelocity(e) {
+    e.preventDefault();
+    let newVelocity = this.refs.velocity_input.getDOMNode().value;
+    VelocityActions.setVelocity(this.props.productId, newVelocity);
+    this.setState({ showVelocityInput: false });
+  },
+
+  onClickVelocity() {
+    this.setState({
+      showVelocityInput: !this.state.showVelocityDropdown
+    }, function() {
+      this.refs.velocity_input.getDOMNode().focus();
+    });
+  },
+
   render: function() {
     return (
       <div className="filters__toolbar container-fluid">
         <div className="col-sm-10">
         <span className="velocity">
           <i className="glyphicon glyphicon-dashboard"></i>
-          <p>{Math.round(this.props.velocity)}</p>
+          { this.state.showVelocityInput ?
+            <form onSubmit={this.changeVelocity} className="velocity__form" ref="velocity_form">
+              <input
+                type="text"
+                ref="velocity_input"
+                defaultValue={this.props.velocity}
+                onSubmit={this.changeVelocity}
+              />
+            </form> :
+            <span onClick={this.onClickVelocity}>{Math.round(this.props.velocity)}</span>
+          }
         </span>
         {_.map(this.props.activeFilters, function(filter, i) {
           return (
--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ require("../app/stores/filters-store-test.js");
 require("../app/stores/product-store-test.js");
 require("../app/views/components/add-item-modal-test.js");
 require("../app/views/components/filters/__tests__/filter-component-test.js");
+require("../app/views/components/filters/filters-toolbar-test.js");
 require("../app/views/components/filters/forms/members-filter-test.js");
 require("../app/views/components/header-test.js");
 require("../app/views/components/item-column/index-test.js");
