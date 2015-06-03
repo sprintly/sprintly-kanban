dd28f5a54f4e3868a6e5af14a3d597d8f0a6085b
Sam Breed <sam@quickleft.com>
2015-06-03 15:18:50 -0700

    correctly remove items from previous collection

--- app/stores/product-store.js
+++ app/stores/product-store.js
@@ -129,11 +129,12 @@ var internals = ProductStore.internals = {
         let previousStatus = model.previous('status');
         let previousCollection = product.getItemsByStatus(previousStatus);
         if (previousCollection) {
-          previousCollection.remove(model);
+          previousCollection.remove(model.id);
         }
         if (collection) {
           collection.add(model);
         }
+        ProductStore.emitChange();
       // }
     });
 
