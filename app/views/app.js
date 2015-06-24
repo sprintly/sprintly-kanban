603a631963029fcc9d6c04d1d7c40a8a30ac8470
Shane Rogers <srogers@quickleft.com>
2015-06-24 16:27:06 -0700

    Prevent double tap zoom events on mobile

--- app/views/app.js
+++ app/views/app.js
@@ -2,17 +2,32 @@ import React from "react";
 import { RouteHandler } from "react-router";
 import Promise from "bluebird";
 import Sidebars from "./components/sidebars";
+import helpers from "./pages/helpers";
 
 export default React.createClass({
-  muteTap(e) {
-    e.preventDefault();
+
+  getInitialState() {
+    return {lastTap: 0}
+  },
+
+  muteDoubleTap(e) {
+    let timeBetweenTaps = e.timeStamp - this.state.lastTap;
+
+    if (timeBetweenTaps < 500 && timeBetweenTaps > 0) {
+      e.preventDefault();
+    }
+    this.setState({
+      lastTap: e.timeStamp
+    })
   },
 
   render: function() {
     let style = {'min-height': `${window.innerHeight}px`};
 
+    let touchEndFn = helpers.isMobile(window) ? this.muteDoubleTap : function() {};
+
     return (
-      <div style={style} className="app-view" onTouchEnd={this.muteTap}>
+      <div style={style} className="app-view" onTouchEnd={this.muteDoubleTap}>
         <RouteHandler {...this.props} />
         <Sidebars {...this.props} />
       </div>
