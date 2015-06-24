7dcf6ae21e1d3034820c9147c86f0279b87d1e0c
Shane Rogers <srogers@quickleft.com>
2015-06-24 15:03:02 -0700

    Mute double tap zoom events

--- app/views/app.js
+++ app/views/app.js
@@ -4,9 +4,13 @@ import Promise from "bluebird";
 import Sidebars from "./components/sidebars";
 
 export default React.createClass({
+  muteTap(e) {
+    e.preventDefault();
+  },
+
   render: function() {
     return (
-      <div className="app-view">
+      <div className="app-view" onTouchEnd={this.muteTap}>
         <RouteHandler {...this.props} />
         <Sidebars {...this.props} />
       </div>
