799236ab3785f95f5cf38c9f608005c0cc74768d
Shane Rogers <srogers@quickleft.com>
2015-07-17 13:09:34 -0700

    Compose format select members to a helper

--- app/views/components/helpers.js
+++ app/views/components/helpers.js
@@ -8,5 +8,24 @@ module.exports = {
         display: `${member.first_name} ${member.last_name}`
       }
     })
-  }
+  },
+  /*
+    TODO: Add test coverage
+  */
+  formatSelectMembers(members) {
+    return _.chain(members)
+            .map(function(member){
+              if (!member.revoked) {
+                return {label: `${member.first_name} ${member.last_name}`, value: member.id}
+              }
+            })
+            .compact()
+            .value()
+  },
+
+  toTitleCase: function(str) {
+    return str.replace(/\w\S*/g, function(text){
+      return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+    });
+  },
 }
