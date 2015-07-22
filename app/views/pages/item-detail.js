1e9052aed499e43bf6690c8d4d165dad5cfe57f1
Shane Rogers <srogers@quickleft.com>
2015-07-22 13:47:03 -0700

    Wire up activities data to the view

--- app/views/pages/item-detail.js
+++ app/views/pages/item-detail.js
@@ -14,6 +14,31 @@ import Slick from 'react-slick';
 import moment from 'moment';
 import OwnerAvatar from '../components/item-card/owner';
 
+const ITEM_CLOSE_MAP = {
+  10: 'invalid',
+  20: 'fixed',
+  30: 'duplicate',
+  40: 'incomplete',
+  50: 'wont fix',
+  60: 'works for me'
+}
+
+const SCORE_TO_SHIRT_SIZES = {
+  0: '~',
+  1: 'S',
+  3: 'M',
+  5: 'L',
+  8: 'XL',
+}
+
+const STATUS_MAP = {
+  5: 'someday',
+  10: 'backlog',
+  20: 'current',
+  30: 'complete',
+  40: 'accepted'
+}
+
 var ItemDetail = React.createClass({
 
   mixins: [State],
@@ -80,13 +105,16 @@ var ItemDetail = React.createClass({
     }
   },
 
+  timeSinceNow(time) {
+    return moment(time).fromNow();
+  },
+
   createdByTimestamp(item) {
     if (item.created_at) {
-      let timeSinceNow = moment(item.created_at).fromNow();
-
+      let timestamp = this.timeSinceNow(item.created_at);
       let creator = `${item.created_by.first_name} ${item.created_by.last_name}`;
 
-      return `Created by ${creator} ${timeSinceNow}`;
+      return `Created by ${creator} ${timestamp}`;
     }
   },
 
@@ -568,36 +596,157 @@ var ItemDetail = React.createClass({
     )
   },
 
+  fieldToValueMap(meta) {
+    let oldVal;
+    let newVal;
+
+    switch (meta.field) {
+      case 'score':
+        oldVal = SCORE_TO_SHIRT_SIZES[meta.old];
+        newVal = SCORE_TO_SHIRT_SIZES[meta.new];
+
+        break;
+      case 'status':
+        oldVal = STATUS_MAP[meta.old];
+        newVal = STATUS_MAP[meta.new];
+
+        break;
+      default:
+        oldVal = meta.old;
+        newVal = meta.new;
+        break;
+    }
+
+    return {
+      oldVal,
+      newVal
+    }
+  },
+
+  itemChanged(meta) {
+    let valueMap = this.fieldToValueMap(meta)
+    let field = helpers.toTitleCase(meta.field);
+    return `${field} from ${valueMap.oldVal} to ${valueMap.newVal}`;
+  },
+
+  itemReassigned(meta) {
+    let from;
+    let to = `to ${this.abbreviatedName(meta.new)}`
+
+    if (meta.old) {
+      from = `from ${this.abbreviatedName(meta.old)}`
+    }
+
+    return [from, to].join(' ');
+  },
+
+  attachmentDescription(meta) {
+    var pre = helpers.vowelSound(meta.type) ? 'An ' : 'A ';
+    let type = helpers.toTitleCase(meta.type);
+
+    return `${pre} ${type}: ${meta.title}`;
+  },
+
+  activityDescription(model) {
+    let meta = model.meta;
+    let description;
+
+    switch (model.action) {
+      case 'item created':
+        description = ''
+        break;
+      case 'item changed':
+        description = this.itemChanged(meta);
+        break;
+      case 'attachment':
+        description = this.attachmentDescription(meta);
+        break;
+      case 'assigned':
+        description = this.itemReassigned(meta);
+        break;
+      default:
+        // This is a hack based on the api not returning an action for a comment
+        if (model.cls === "Comment") {
+          description = model.body
+        } else {
+          description = `DESCRIPTION CASE NOT HANDLED: cls:${model.cls}, label:${model.label}`
+        }
+    }
+
+    return description;
+  },
+
+  activityTypeMap(action) {
+    const ACTIVITY_TYPES = {
+      'item created': 'created this',
+      'item changed': 'changed',
+      'attachment': 'attached',
+      'assigned': 'reassigned',
+      '': 'commented'
+    }
+
+    return ACTIVITY_TYPES[action] || `WARN: ${action} ACTIVITY`;
+  },
+
+  abbreviatedName(model) {
+    if (!model) {
+      return '';
+    } else {
+      return `${model.first_name} ${model.last_name.charAt(0)}.`
+    }
+  },
+
   activity() {
-    // let activityItems = _.chain(_.times(15)).map(function(n){
-    //   return (
-    //     <li className="comment">
-    //       <div className="avatar no-gutter">
-    //         <Gravatar email={"srogers@quickleft.com"} size={30} />
-    //       </div>
-    //       <div className="col-md-2 creator">
-    //         Shane Rogers
-    //       </div>
-    //       <div className="col-md-8 description">
-    //         Justin here are the mockups for this story https://www.invision.co...
-    //       </div>
-    //       <div className="timestamp pull-right">
-    //         5 mins ago
-    //       </div>
-    //     </li>
-    //   )
-    // }).value();
-    //
-    // return (
-    //   <div className="col-md-12 section activity">
-    //     <div className="col-md-12">
-    //       {this.header('activity')}
-    //       <ul>
-    //         {activityItems}
-    //       </ul>
-    //     </div>
-    //   </div>
-    // )
+    let activityItems;
+    let activity = this.state.item.activity;
+
+    if (activity) {
+      let totalActivityCount = activity.total_count || 0;
+
+      if (activity.activities && this.props.members.length) {
+
+        activityItems = _.map(activity.activities, _.bind(function(model) {
+          let creator = _.findWhere(this.props.members, {id: model.user});
+          let creatorEmail = creator.email;
+          let creatorName = this.abbreviatedName(creator);
+          let activityType = this.activityTypeMap(model.action);
+          let description = this.activityDescription(model);
+          let timestamp = this.timeSinceNow(model.created);
+
+          return (
+            <li className="comment">
+              <div className="avatar no-gutter">
+                <Gravatar email={creatorEmail} size={30} />
+              </div>
+              <div className="col-md-2 creator no-wrap-truncate">{creatorName}</div>
+              <div className="col-md-1 activity-type no-gutter">{activityType}</div>
+              <div className="col-md-7 description">
+                {description}
+              </div>
+              <div className="timestamp pull-right">
+                {timestamp}
+              </div>
+            </li>
+          )
+        },this))
+      } else {
+        activityItems = <li className="comment">No Activity Yet</li>
+      }
+
+      return (
+        <div className="col-md-12 section activity">
+          <div className="col-md-12">
+            <div className="header">
+              <div className="title">{helpers.toTitleCase('activity')}</div>
+              <div className="activity__counter">{totalActivityCount}</div>
+            </div>
+            <ul>
+              {activityItems}
+            </ul>
+          </div>
+        </div>
+      )
+    }
   },
 
   // Display some sort of loading state via the item store
