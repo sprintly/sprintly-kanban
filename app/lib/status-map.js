088a2fc342ee9ace102dcd31a2bb062f70ba0c4d
Sam Breed <sam@quickleft.com>
2015-07-27 11:37:57 -0700

    adding a global map of item statuses

---
+++ app/lib/status-map.js
@@ -0,0 +1,10 @@
+const STATUSES = {
+  'someday': 'Someday',
+  'backlog': 'Backlog',
+  'in-progress': 'Current',
+  'completed': 'Complete',
+  'accepted': 'Accepted'
+};
+
+export default STATUSES;
+
