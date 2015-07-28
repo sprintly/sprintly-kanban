3b8f2b4730a76638a43336c90ca927d912d4722a
Shane Rogers <srogers@quickleft.com>
2015-07-27 17:29:53 -0700

    Compose item-detail attachments to own component

---
+++ app/views/pages/item-detail/index.js
@@ -0,0 +1,169 @@
+import React from 'react/addons';
+import _ from 'lodash';
+
+// Stores
+import ProductStore from '../../../stores/product-store';
+import ItemActions from '../../../actions/item-actions';
+// Components
+import ItemDetails from './item-details';
+import ItemDescription from './item-description';
+import ItemAttachments from './item-attachments';
+import ItemSubitems from './item-subitems';
+import ItemComments from './item-comments';
+import ItemActivity from './item-activity';
+// Libs
+import {State,Link} from 'react-router';
+
+var ItemDetail = React.createClass({
+
+  mixins: [State],
+
+  getInitialState() {
+    let bodyHeight = document.body.getBoundingClientRect().height;
+    let headerHeight = document.getElementsByClassName('product__header-menu')[0].getBoundingClientRect().height;
+    let detailHeight = bodyHeight - headerHeight;
+
+    return {
+      item: {},
+      attachmentsPanel: false,
+      itemDetailHeight: detailHeight,
+      descriptionEditable: false,
+    };
+  },
+  // Display some sort of loading state via the item store
+  componentDidMount() {
+    ProductStore.addChangeListener(this._onChange);
+    ItemActions.fetchItem(this.getParams().id, this.getParams().number);
+    // TODO: Fetch the item followers on component did mount
+
+    ItemActions.fetchActivity(this.getParams().id, this.getParams().number);
+  },
+
+  componentWillUnmount() {
+    ProductStore.removeChangeListener(this._onChange);
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if (this.getParams().number != this.state.item.number) {
+      ItemActions.fetchItem(this.getParams().id, this.props.number);
+      ItemActions.fetchActivity(this.getParams().id, this.getParams().number);
+      this._onChange();
+    }
+  },
+
+  setItem(key, ev, value) {
+    let item = _.cloneDeep(this.state.item);
+    item[key] = value;
+
+    this.setState({item: item});
+  },
+
+  updateItem() {
+    let productId = this.getParams().id;
+    let itemId = this.getParams().number;
+
+    ProductActions.updateItem(productId, itemId, { description: this.state.item.description });
+  },
+
+  // *********** Convenience Line
+
+  itemDetails() {
+    return (
+      <ItemDetails  members={this.props.members}
+                       type={this.state.item.type}
+                      title={this.state.item.title}
+                        who={this.state.item.who}
+                       what={this.state.item.what}
+                        why={this.state.item.why}
+                     number={this.state.item.number}
+                       tags={this.state.item.tags}
+                  createdAt={this.state.item.created_at}
+                  createdBy={this.state.item.created_by}
+                     status={this.state.item.status}
+                     score={this.state.item.score}
+                   assignee={this.state.item.assigned_to} />
+    )
+  },
+
+  itemDescription() {
+    return (
+      <ItemDescription followers={[]}
+                     description={this.state.item.description}
+                         setItem={this.setItem} />
+    )
+  },
+
+  itemAttachments() {
+    return (
+      <ItemAttachments attachments={this.state.attachments} />
+    )
+  },
+
+  render() {
+    let subitems;
+    if (!this.state.item.number) {
+      // Update to loading state where correct
+      return <div/>;
+    }
+
+    let stripeClass = `stripe ${this.state.item.type}`;
+    let closeClass = `item-detail__close ${this.state.item.type}`;
+    var stripeStyles = {height: `${this.state.itemDetailHeight}px`};
+
+    let itemSubitems;
+    let itemDetails = this.itemDetails()
+    let itemDescription = this.itemDescription();
+    let itemAttachments = this.itemAttachments();
+    // let itemComments = <ItemComments />
+    // let itemActivity = <ItemActivity />
+    // if (this.state.item.type == 'story') {
+    //   itemSubitems = <ItemSubitems />
+    // }
+    // {itemDescription}
+    // {itemAttachments}
+    // {itemSubitems}
+    // {itemComments}
+    // {itemActivity}
+
+    return (
+      <div ref="itemDetail" className="container-fluid item-detail no-gutter">
+        <div style={stripeStyles} className={stripeClass}>
+          <Link to="product" params={{ id: this.getParams().id }} className={closeClass}>
+            <span aria-hidden="true" className="glyphicon glyphicon-menu-right"/>
+          </Link>
+        </div>
+        <div className="content">
+          {itemDetails}
+          {itemDescription}
+          {itemAttachments}
+        </div>
+      </div>
+    )
+  },
+
+  _onChange() {
+    let item = ProductStore.getItem(this.getParams().id, this.getParams().number);
+
+    if (item) {
+
+      let extendedState = {};
+      let subitemsLength = item.sub_items.length;
+
+      if (!this.state.subitems && subitemsLength) {
+        var subitemState = _.map(new Array(subitemsLength), () => { return false});
+        extendedState['subitems'] = subitemState
+      }
+
+      let attachments = {};
+      if (item.activity && item.activity.activities) {
+        extendedState['attachments'] = _.where(item.activity.activities, {'action':'attachment'});
+      } else {
+        extendedState['attachments'] = [];
+      }
+
+      this.setState(_.extend({item}, extendedState));
+    }
+  }
+})
+
+export default ItemDetail
---
+++ app/views/pages/item-detail/item-attachments.js
@@ -0,0 +1,206 @@
+import React from 'react/addons';
+import _ from 'lodash';
+import helpers from '../../components/helpers';
+import ItemDetailMixin from './detail-mixin';
+import {State} from 'react-router'
+import ProductActions from '../../../actions/product-actions';
+
+const CarouselSettings = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  speed: 500,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
+var ItemAttachments = React.createClass({
+
+  mixins: [State, ItemDetailMixin],
+
+  getInitialState() {
+    return {
+      panelOpen: false
+    }
+  },
+
+  propTypes() {
+    attachments: React.PropTypes.array
+  },
+
+  togglePanel() {
+    this.setState({panelOpen: !this.state.panelOpen});
+  },
+
+  counts(imageCount, fileCount) {
+    let counts = [];
+
+    if (!imageCount && !fileCount) {
+      counts.push(<li>No Attachments</li>)
+    }
+
+    if (imageCount) {
+      counts.push(<li>Images: {imageCount}</li>)
+    }
+    if (fileCount) {
+      counts.push(<li>Files: {fileCount}</li>);
+    }
+
+    return counts;
+  },
+
+  attachmentsHeader(imagesCount, filesCount) {
+    let counts = this.counts(imagesCount, filesCount);
+    let headerClasses = React.addons.classSet({
+      'header': true,
+      'open': this.state.panelOpen
+    });
+    let toggleClasses = React.addons.classSet({
+      'toggle': true,
+      'transparent': !this.props.attachments.length
+    })
+    let caretClasses = `glyphicon glyphicon-menu-${this.caretState()}`;
+
+    return (
+      <div className={headerClasses}>
+        <a className={toggleClasses} onClick={this.togglePanel}>
+          <span aria-hidden="true" className={caretClasses}/>
+        </a>
+        <div className="sep-vertical"></div>
+        <div className="title">Attachments</div>
+        <ul className="attachments__info-list">{counts}</ul>
+      </div>
+    )
+  },
+
+  attachmentsViewer() {
+    if (this.props.attachments) {
+      var attachments = _.map(this.images(), (image, i) => {
+                          // Temp Greyscale before authed asset use
+                          var styles = {
+                            width: '33%',
+                            height: '170px',
+                            'background-color': `RGBA(69,69,69, ${parseFloat((Math.random() * (0.120 - 0.0200) + 0.0200))})`,
+                          }
+                          // background: `url(${image.meta.permalink}) no-repeat`
+
+                          return (
+                            <div className="attachment-slide" key={i} style={styles} onClick={this.showAttachment}></div>
+                          );
+                        });
+
+      return (
+        <Slick {...CarouselSettings}>
+          {attachments}
+        </Slick>
+      );
+    }
+  },
+
+  showAttachment() {
+    console.log('Implement attachment viewer: ');
+  },
+
+  caretState() {
+    return this.state.panelOpen ? 'down' : 'right'
+  },
+
+  fileList(files) {
+    if (files) {
+      return _.map(files, function(file, i) {
+        return (
+          <li key={i}>
+            <a className="attachment-link">{file.meta.title}</a>
+          </li>
+        )
+      })
+    } else {
+      return (<li><a className="attachment-link">No files attached</a></li>)
+    }
+  },
+
+  images() {
+    return _.chain(this.props.attachments)
+                    .map((attachment) => {
+                      if (this.imageFileExt(attachment.meta.url)) {
+                        return attachment;
+                      }
+                    })
+                    .compact()
+                    .value();
+  },
+
+  imageFileExt(url) {
+    return (/\.(gif|jpg|jpeg|tiff|png)$/i).test(url);
+  },
+
+  render: function() {
+    console.log('ATTACHMENTS');
+
+    // let attachmentViewer = this.attachmentsViewer();;
+    // {attachmentViewer}
+
+    let images = this.images();
+    let files = _.difference(this.props.attachments, images);
+    let fileList = this.fileList(files);
+    let header = this.attachmentsHeader(images.length, files.length);
+    let contentClasses = React.addons.classSet({
+      'content': true,
+      'open': this.state.panelOpen
+    });
+
+    return (
+      <div className="col-md-12 section attachments">
+        <div className="col-md-12">
+          {header}
+          <div className={contentClasses}>
+            <div className="col-md-12 attachments__internals">
+              <div className="col-md-9">
+                <div className="title">
+                  Images: {images.length}
+                </div>
+                <div className="attachments-viewer">
+                </div>
+              </div>
+              <div className="attachments__links">
+                <div className="title">
+                  Files: {files.length}
+                </div>
+                <div className="sep"></div>
+                <ul className="links">
+                  {fileList}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+});
+
+export default ItemAttachments;
