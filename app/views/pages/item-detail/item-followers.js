da972e6a9aa84107e269609ee2689a983ed77dc7
Shane Rogers <srogers@quickleft.com>
2015-07-29 18:21:47 -0700

    Compose followers component

---
+++ app/views/pages/item-detail/item-followers.js
@@ -0,0 +1,49 @@
+import React from 'react/addons';
+import _ from 'lodash';
+import ItemDetailMixin from './detail-mixin';
+import {State} from 'react-router'
+import ProductActions from '../../../actions/product-actions';
+
+var ItemFollowers = React.createClass({
+
+  mixins: [State, ItemDetailMixin],
+
+  propTypes: {
+    followers: React.PropTypes.array,
+    setItem: React.PropTypes.func
+  },
+
+  followItem() {
+    console.log('Follow the item');
+  },
+
+  followerNodes() {
+    if (this.props.followers) {
+      return _.map(this.props.followers, (follower, i) => {
+        return (
+          <div key={i} className="col-md-4">
+            <li><Gravatar email={follower.email} size={36} /></li>
+          </div>
+        )
+      })
+    } else {
+      return <div className="no-followers">No followers of this item yet</div>
+    }
+  },
+
+  render() {
+    let followerNodes = this.followerNodes();
+
+    return (
+      <div className="col-md-3 followers">
+        {this.header('followers')}
+        <ul>
+          {followerNodes}
+        </ul>
+        <button className="detail-button kanban-button-secondary" onClick={this.followItem}>Follow</button>
+      </div>
+    )
+  }
+})
+
+export default ItemFollowers;
