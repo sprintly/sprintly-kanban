2d8fb10ef1d614b8d575327be51d1ebf34429c5d
Hargobind S. Khalsa <hkhalsa@quickleft.com>
2015-09-16 16:08:53 -0600

    Use runtime configuration for javascript

--- app/config.js
+++ app/config.js
@@ -1,7 +1,10 @@
 /*eslint-env node */
-export const CHANNEL_PREFIX = process.env.NODE_ENV === 'production' ?
-  'api-product_sprintly-aws' : 'api-product_sprintly-development-justinlilly';
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://sprint.ly' : 'https://local.sprint.ly:9000';
+if (typeof __manifold_config !== "object") {
+  throw new Error("__manifold_config is missing!");
+}
 
-export const PUSHER_KEY = process.env.PUSHER_KEY || '';
+export default __manifold_config;
+export const BASE_URL = __manifold_config.BASE_URL;
+export const PUSHER_KEY = __manifold_config.PUSHER_KEY;
+export const CHANNEL_PREFIX = __manifold_config.CHANNEL_PREFIX;
