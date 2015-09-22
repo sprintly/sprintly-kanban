ac55af393803d03216155d89cc1fdcbbdf6512ef
Sam Breed <sam@quickleft.com>
2015-09-22 14:48:17 -0700

    make the config changes pass through to the app properly

--- app/config.js
+++ app/config.js
@@ -1,11 +1,11 @@
 /*eslint-env node */
 
-if (typeof __manifold_config !== "object") {
+if (typeof window.__manifold_config !== "object") {
   console.warn("__manifold_config is missing!");
   var __manifold_config = {};
 }
 
-export default __manifold_config;
-export const BASE_URL = __manifold_config.BASE_URL;
-export const PUSHER_KEY = __manifold_config.PUSHER_KEY;
-export const CHANNEL_PREFIX = __manifold_config.CHANNEL_PREFIX;
+export default window.__manifold_config;
+export const BASE_URL = window.__manifold_config.BASE_URL;
+export const PUSHER_KEY = window.__manifold_config.PUSHER_KEY;
+export const CHANNEL_PREFIX = window.__manifold_config.CHANNEL_PREFIX;
